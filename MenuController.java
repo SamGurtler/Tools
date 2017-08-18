b910a2941b55814d69fafccb2c3e9644b352e615
Samuel C. Gurtler <StudentcareerSamgurtler@gmail.com>
2017-08-18 00:01:22 -0700

    Add files via upload

--- MenuController.java
+++ MenuController.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -19,10 +18,8 @@ import javafx.scene.input.InputMethodEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
 public class MenuController implements Initializable{
-    @FXML
+	@FXML 
     private ToggleGroup Mode;
     @FXML
     private HBox pu;
@@ -36,10 +33,9 @@ public class MenuController implements Initializable{
     private TreeView showFile;
     @FXML
     private ProgressBar progressBar;
-    @FXML
-    private CheckBox checkZipmode;
     private long num=0L;
-    private boolean en=true;
+    private boolean en,zipMode=true;
+    private TreeItem<String> root=new TreeItem<String>("Files and Folders");
     @FXML
     public void en(ActionEvent event){
     }
@@ -77,33 +73,51 @@ public class MenuController implements Initializable{
     }
     @FXML
     public void add(ActionEvent event) {
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("./fFOptions.fxml"));
+    	FXMLLoader loader=new FXMLLoader(getClass().getResource("./fFOptions.fxml"));
         try {
             FFOptionsController CntrllrVBx=new FFOptionsController();
+            loader.setController(CntrllrVBx);
             VBox vBox=(VBox)loader.load();
-            Stage fFStage= (Stage)pu.getScene().getWindow();
-            Scene tmp=fFStage.getScene();
-            vBox.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST,(WindowEvent event1)-> {
-            fFStage.setScene(tmp);
-            System.out.println("HUH!");
-            //Supposed to add file paths into the Treeview object called showFile
-            //TreeItem<String> root=new TreeItem<>();
-            //for(File f : CntrllrVBx.getFiles())root.getChildren().add(new TreeItem<>(f.toString()));
-            });
-            fFStage.setScene(new Scene(vBox));
+            Stage optionStage=new Stage();
+            optionStage.setScene(new Scene(vBox));
+            optionStage.showAndWait();
+            for(File f:CntrllrVBx.getFiles()) {
+            	if(f.isDirectory())root.getChildren().add(listFilesAndFilesSubDirectories(f.getAbsolutePath()));
+            	else root.getChildren().add(new TreeItem<String>(f.getName()));
+            }
+            treeUpdate();
         } catch (IOException ex) {
-            //Logger.getLogger(MenuController.class.getName()).log(Level.INFO, null, ex);
+            Logger.getLogger(MenuController.class.getName()).log(Level.INFO, null, ex);
         }
+    }
+    //Modified from https://dzone.com/articles/java-example-list-all-files
+    public TreeItem<String> listFilesAndFilesSubDirectories(String directoryName){
+        File directory = new File(directoryName);
+        TreeItem<String> branch=new TreeItem<String>(directoryName);
+        File[] fList = directory.listFiles();
+        for (File file : fList){
+            if (file.isFile()){
+            	branch.getChildren().add(new TreeItem<String>(file.getAbsolutePath()));
+            } else if (file.isDirectory()){
+                branch.getChildren().add(listFilesAndFilesSubDirectories(file.getAbsolutePath()));
+            }
+        }
+        return branch;
+    }
+    private void treeUpdate() {
+    	showFile.setRoot(root);
     }
     @FXML
     public void remove(ActionEvent event){
     }
     @FXML
     public void setZipMode(ActionEvent event){
+    	zipMode=!zipMode;
     }
     @Override
     public void initialize(URL location, ResourceBundle resources){
-        showFile=new TreeView<String>(new TreeItem<String>("Files and Folders"));
+        root.setExpanded(true);
+    	showFile.setRoot(root);
         makeKeys();
     }
 }
